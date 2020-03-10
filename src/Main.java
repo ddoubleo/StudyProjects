c2753f8ca96ddbf4bc1b72a345689ba11e628234
ddoubleo <sergienko.ni@edu.spbstu.ru>
2020-03-10 10:24:59 +0300

    it works!!!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         ticTacToe first = new ticTacToe();
+        first.print();
+        first.add(true, 1, 1);
+        first.add(true, 2, 2);
+        first.add(true, 2, 3);
+        first.add(false, 1, 3);
+        first.clear(2, 3);
+        first.add(true, 3, 3);
+        first.print();
+        first.longestCross();
+
     }
 }
--- src/ticTacToe.java
+++ src/ticTacToe.java
@@ -1,43 +1,124 @@
+import java.util.Arrays;
+
 class ticTacToe {
     private char[][] game;
     private int size;
     private int longestSequence = 0;
 
     ticTacToe() {
-        int size = 3;
+        size = 3;
         game = new char[size][size];
-    }
-
-    private void add(boolean flag, int xCoordinate, int yCoordinate) {
-        if (flag) game[xCoordinate][yCoordinate] = '✕';
-        else game[xCoordinate][yCoordinate] = 'O';
-    }
-
-    private void clear(int xCoordinate, int yCoordinate) {
-        game[xCoordinate][yCoordinate] = 0;
-    }
-
-    private String longestCross() {
-        int longestCross = 0;
-        int count = 0;
         for (int i = 0; i < size; i++) {
-            if (game[i][0] == '✕') {
-                for (int i1 = i + 1; i < size; i++) { //horizontal
-                    if (game[i1][0] == game[i1 - 1][0]) count++;
-                    if (count > longestCross) longestCross = count;
-                }
-                for (int j1 = 1; j1 < size; j1++) { //vertical
-                    if (game[i][j1] == game[i][j1 - 1]) count++;
-                    if (count > longestCross) longestCross = count;
-                }
-
-            } else continue;
+            for (int j = 0; j < size; j++) {
+                game[i][j] = '-';
+            }
         }
-        return "123";
     }
 
-    public String horizontal() {
+    void print() {
+        System.out.println(Arrays.deepToString(game)
+                .replace("],", "\n").replace(",", "\t| ")
+                .replaceAll("[\\[\\]]", " "));
+        System.out.println("\n");
     }
-    public String vertical {}
-    public String diagonal {}
+
+    void add(boolean flag, int xCoordinate, int yCoordinate) {
+        if (flag) game[yCoordinate - 1][xCoordinate - 1] = '✕';
+        else game[yCoordinate - 1][xCoordinate - 1] = 'O';
+    }
+
+     void clear(int xCoordinate, int yCoordinate) {
+        game[yCoordinate - 1][xCoordinate - 1] = '-';
+    }
+
+    int[] longestCross() {
+        int[] kostil = {0,0,0,0};
+        int[] result = new int[0];
+        int[] res = new int[0];
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - 1; j++) {
+                if (game[i][j] == '✕') res = scan(i, j);
+                if (!Arrays.equals(kostil,res)) result = res;
+            }
+        }
+        System.out.println(Arrays.toString(result));
+        System.out.print(longestSequence);
+        return result;
+
+    }
+
+    private int[] longestNull() {
+        int[] result = new int[5];
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - 1; j++) {
+                if (game[i][j] == 'O') result = scan(i, j);
+                result[4] = longestSequence;
+            }
+        }
+        return result;
+    }
+
+    private int[] scan(int x, int y) {
+        int[] result = new int[4];
+        int count = 1;
+        for (int i1 = x + 1; i1 < size; i1++) { //horizontal
+            if (game[i1][y] == game[i1 - 1][y]) {
+                count++;
+                if (count > longestSequence) {
+                    longestSequence = count;
+                    result[0] = x+1;
+                    result[1] = y+1;
+                    result[2] = i1+1;
+                    result[3] = y+1;
+                }
+            } else count = 1;
+
+        }
+        count = 1;
+        for (int i1 = y + 1; i1 < size; i1++) { //vertical
+            if (game[x][i1] == game[x][i1 - 1]) {
+                count++;
+                if (count > longestSequence) {
+                    longestSequence = count;
+                    result[0] = x+1;
+                    result[1] = y+1;
+                    result[2] = x+1;
+                    result[3] = i1+1;
+                }
+            } else count = 1;
+
+
+        }
+        count = 1;
+        for (int i1 = x + 1, j1 = y + 1; i1 < size && j1 < size; j1++, i1++) { //diagonal1
+            if (game[i1][j1] == game[i1 - 1][j1 - 1]) {
+                count++;
+                if (count > longestSequence) {
+                    longestSequence = count;
+                    result[0] = x+1;
+                    result[1] = y+1;
+                    result[2] = i1+1;
+                    result[3] = j1+1;
+                }
+            } else count = 1;
+
+
+        }
+        count = 1;
+        for (int i1 = x - 1, j1 = y + 1; i1 > 0 && j1 < size; j1++, i1--) { //diagonal2
+            if (game[i1][j1] == game[i1 + 1][j1 - 1]) {
+                count++;
+                if (count > longestSequence) {
+                    longestSequence = count;
+                    result[0] = x+1;
+                    result[1] = y+1;
+                    result[2] = i1+1;
+                    result[3] = j1+1;
+                }
+            } else count = 1;
+        }
+
+        return result;
+    }
+
 }
